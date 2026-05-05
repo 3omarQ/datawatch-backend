99798378fe74238d01def85e6a1454fcd31441b9
3omarQ <omarkassar.202@gmail.com>
2026-05-05 16:30:31 +0100

    change render-build.sh order

--- render-build.sh
+++ render-build.sh
@@ -1,26 +1,11 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Install dependencies (without running postinstall scripts that download Chrome)
 npm install
-
-# Build your NestJS app
 npm run build
 
-# Set the Puppeteer cache directory to a path Render persists
 PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
 mkdir -p $PUPPETEER_CACHE_DIR
 
-# Install Chrome into that directory
+# Always install Chrome fresh into the persistent cache path
 npx puppeteer browsers install chrome
-
-# Sync cache to/from build cache dir
-if [[ ! -d /opt/render/project/src/.cache/puppeteer/chrome ]]; then
-  echo "...Storing Puppeteer Cache in Build Cache"
-  mkdir -p /opt/render/project/src/.cache/puppeteer
-  cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/
-else
-  echo "...Copying Puppeteer Cache from Build Cache"
-  cp -R /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
-fi
